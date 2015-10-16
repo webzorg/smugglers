7772a5e69583a3f37f2949dacc16f0e3a234779c
Lasha <labulashvili@gmail.com>
2015-10-16 13:29:20 +0400

    added GA

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,4 +76,8 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  GA.tracker = "UA-68945335-1"
+  GA.script_source = :doubleclick
+
 end
