f056f88a069824ff1d22ca445284457256b1b0e6
webgen <webgen@outlook.com>
2015-10-28 00:31:07 +0400

    branching

--- config/routes.rb
+++ config/routes.rb
@@ -6,8 +6,8 @@ Rails.application.routes.draw do
         post :import
       end
     end
-    resources :users
-    resources :sessions
+    resources :users, only: [:new, :create]
+    resources :sessions, only: [:new, :create, :destroy]
     root 'home#index'
   end
 
