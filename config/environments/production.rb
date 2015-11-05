933e5643c34eb2dae7200fbc28e47453871bb4d5
webgen <webgen@outlook.com>
2015-11-05 13:38:16 +0400

    removed asset prefix

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -60,9 +60,8 @@ Rails.application.configure do
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.action_controller.asset_host = 'http://assets.example.com'
   config.action_controller.asset_host = "//#{ENV['FOG_DIRECTORY']}.s3.amazonaws.com"
-  config.assets.prefix = "/production/assets"
   config.assets.initialize_on_precompile = true
-  
+
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
